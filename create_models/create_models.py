ea059377dc1a64581b99c1f0aaab2ce267989626
40gilad <40gilad.shenkar@gmail.com>
2024-04-24 13:24:15 +0300

    changed filters and globalMaxPooling in resnet_50_small_conv

--- create_models/create_models.py
+++ create_models/create_models.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Input, Conv2D, Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Input, Conv2D, Dense, GlobalAveragePooling2D, GlobalMaxPooling2D, Dropout, Flatten
 from tensorflow.keras.applications import resnet50
 from tensorflow.keras.models import Model
 
@@ -37,8 +37,8 @@ def create_small_resnet50_with_conv(input_shape, lr, n_classes):
     input = Input(shape=input_shape)
     x = resnet50.preprocess_input(input)
     x = base_model(x)
-    x = Conv2D(filters=32,kernel_size=(1,1))(x)
-    x = GlobalAveragePooling2D()(x)
+    x = Conv2D(filters=16,kernel_size=(1,1))(x)
+    x = GlobalMaxPooling2D()(x)
     x = Dense(n_classes, activation='softmax')(x)
     model = Model(inputs=input, outputs=x)
     model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
