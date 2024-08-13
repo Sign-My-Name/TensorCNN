7c6b03b36f29db5ed6ed5b86146e111d2f70bc02
40gilad <40gilad.shenkar@gmail.com>
2024-08-13 18:33:21 +0300

    process images now goes to trn and tst

--- create_metadata.py
+++ create_metadata.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import os
-import numpy as np
 from pathlib import Path
 from datetime import datetime
-from sklearn.model_selection import train_test_split
-import mediapipe as mp
-import cv2
+
 
 english_to_hebrew = {
     'B': 'ב', 'C': 'כ', 'D': 'ו', 'F': 'ט', 'I': 'י', 'L': 'ל', 'M': 'מ', 'N': 'נ', 'R': 'ר', 'S': 'ס',
@@ -18,6 +15,9 @@ hebrew_dict = {
     'ע': 15, 'פ': 16, 'צ': 17, 'ק': 18, 'ר': 19, 'ש': 20, 'ת': 21
 }
 
+words_dict = {
+    'bye': 0, 'excellent': 1, 'idf': 2, 'ok': 3, 'soldier': 4
+}
 hehbrew_letters_in_english = {
     'ale': 'א', 'bet': 'ב', 'gim': 'ג', 'dal': 'ד', 'hey': 'ה', 'vav': 'ו', 'zay': 'ז', 'het': 'ח', 'tet': 'ט',
     'yud': 'י'
@@ -42,8 +42,8 @@ def create_tst_df(images_dir, _class_encoding=None, weight=1):
     eng_heb_list = []
     weights = []
     for img in os.listdir(images_dir):
-        #cur_label = img.split('_')[0]
-        cur_label=img.split('.')[0] # ain.jpeg for example
+        # cur_label = img.split('_')[0]
+        cur_label = img.split('.')[0]  # ain.jpeg for example
         if len(cur_label) > 3:  # not a letter
             continue
         images_list.append(os.path.join(images_dir, img))
@@ -86,7 +86,7 @@ def create_trn_df(images_dir, _class_encoding=None, weight=1):
         if _class_encoding is None:  # creates class encoding. Change this if you dont want class encoding to be from counter
             class_encoding[dir] = hebrew_dict[hehbrew_letters_in_english[dir]]
             class_counter += 1
-            #class_encoding[dir] = class_counter
+            # class_encoding[dir] = class_counter
         for img in os.listdir(os.path.join(images_dir, dir)):
             images_list.append(os.path.join(images_dir, dir, img))
             labels_list.append(dir)
@@ -102,6 +102,69 @@ def create_trn_df(images_dir, _class_encoding=None, weight=1):
         return df, class_encoding
     else:
         return df
+
+
+def create_words_trn_df(images_dir, _class_encoding=None, weight=1):
+    """
+    Assums that in imgs_dir there are dirs named by the letter only (len=1)
+    :param imgs_dir:
+    :return: metadata df. if _class_encoding is None, returns class_encoding dict
+    """
+    df = pd.DataFrame()
+    images_list = []
+    labels_list = []
+    weights = []
+    lable_class_encoding = []
+
+    if _class_encoding is None:
+        class_encoding = {}
+    else:
+        class_encoding = _class_encoding
+
+    for dir in os.listdir(images_dir):  # dir is the letter
+        if _class_encoding is None:  # creates class encoding. Change this if you dont want class encoding to be from counter
+            class_encoding[dir] = words_dict[dir]
+        for img in os.listdir(os.path.join(images_dir, dir)):
+            images_list.append(os.path.join(images_dir, dir, img))
+            labels_list.append(dir)
+            lable_class_encoding.append(class_encoding[dir])
+            weights.append(weight)
+    df["filename"] = images_list
+    df["label"] = labels_list
+    df['class_encoding'] = lable_class_encoding
+    df['weights'] = weights
+    if _class_encoding is None:
+        return df, class_encoding
+    else:
+        return df
+
+
+def create_words_tst_df(images_dir, _class_encoding=None, weight=1):
+    """
+    assums that all tst images are in the same directory (no subdirs) and each img name starts with the letter it represent and then underline
+    for example, an img represent the letter B will be named B_something
+    :param images_dir:
+    :param _class_encoding:
+    :return:
+    """
+    df = pd.DataFrame()
+    images_list = []
+    labels_list = []
+    lable_class_encoding = []
+    weights = []
+    for img in os.listdir(images_dir):
+        cur_label = img.split('.')[0]  # ain.jpeg for example
+        images_list.append(os.path.join(images_dir, img))
+        labels_list.append(cur_label)
+        #lable_class_encoding.append(class_encoding[cur_label])
+        weights.append(weight)
+
+    df["filename"] = images_list
+    df["label"] = labels_list
+    df['class_encoding'] = lable_class_encoding
+    df['weights'] = weights
+
+    return df
 
 
 def create_new_dirs(subdir=None):
@@ -133,31 +196,21 @@ def save_df(df, path, name):
 
 
 if __name__ == '__main__':
-    images_dir = Path(r'C:\Users\40gil\Desktop\final_project\tensor_training\processed_images\half_landmarks')
-    subdir_name = f'Sivan_{datetime.now().strftime("%m%Y%d-%H%M")}'
-    new_dir = create_new_dirs(subdir=subdir_name)
+    images_dir = Path(r'C:\Users\40gil\Desktop\AltDegree\final_project\tensor_training\processed_images\sivan_example_processed')
+    new_dir_name = f'SivanLettersExample{datetime.now().strftime("%m%Y%d-%H%M")}'
+    new_dir = create_new_dirs(subdir=new_dir_name)
     TRdfs = []
 
     # region train dfs
-    TRdf, class_encoding = create_trn_df(images_dir=images_dir / 'sivan')
+    TRdf, class_encoding = create_trn_df(images_dir=images_dir / 'tst')
     TRdfs.append(TRdf)
-    # TRdf = create_trn_df(images_dir=images_dir / 'arabic_to_english', _class_encoding=class_encoding)
-    # TRdfs.append(TRdf)
-
     # endregion
 
     # region test df
-    TSdfs = []
-    TSdf = create_tst_df(images_dir=images_dir / 'sivan_tst', _class_encoding=class_encoding)
-    TSdfs.append(TSdf)
-    #TSdf = create_trn_df(images_dir=images_dir / 'hebrew_to_english', _class_encoding=class_encoding,weight=15)
-    # ----- add 80% friends to train
-    # freinds_to_train = TSdf.sample(frac=0.8, random_state=666,ignore_index=False)
-    #friends_to_train, TSdf = train_test_split(TSdf, test_size=0.2, stratify=TSdf.class_encoding,random_state=666)
-    #TRdfs.append(friends_to_train)
-    # ------
-    #TSdfs.append(TSdf)
+    # TSdfs = []
+    # TSdf = create_words_tst_df(images_dir=images_dir / 'tst')
+    # TSdfs.append(TSdf)
     save_df(df=TRdfs, path=new_dir, name='trn_metadata')
-    save_df(df=TSdfs, path=new_dir, name='tst_metadata')
+    # save_df(df=TSdfs, path=new_dir, name='tst_metadata')
 
     # endregion
